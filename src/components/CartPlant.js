7bd6d12f155edf567201e110200245d94322958a
Amine Benchaaboun <amine.benchaaboun@gmail.com>
2025-04-07 19:45:14 +0100

    add styles on button

--- src/components/CartPlant.js
+++ src/components/CartPlant.js
@@ -1,15 +1,24 @@
 import Trash from "../assets/trash.svg";
 import { addToCart, delFromCart } from "../Utils";
+import "../styles/CartPlant.css";
 export function CartPlant({ name, price, amount, cart, setCart }) {
   return (
     <div>
       {name} {price}€
-      <div>
-        <button onClick={() => delFromCart(cart, setCart, name)}>
-          {amount === 1 ? <img src={Trash}></img> : <span>-</span>}
+      <div className="lmj-amount-cart-plant">
+        <button
+          className="lmj-button-cart-plant"
+          onClick={() => delFromCart(cart, setCart, name)}
+        >
+          {amount === 1 ? <img className="trash-icon"src={Trash}></img> : <span>-</span>}
         </button>
         x{amount}
-        <button onClick={() => addToCart(cart, setCart, name, price)}>+</button>
+        <button
+          className="lmj-button-cart-plant"
+          onClick={() => addToCart(cart, setCart, name, price)}
+        >
+          +
+        </button>
       </div>
     </div>
   );
